92fc76ddd8b15def096ba78b2ac341160a355204
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2023-05-19 17:42:59 -0300

    continuacao do curso

---
+++ AULAS/Variaveis/8_console.log/scripts.js
@@ -0,0 +1,20 @@
+/*Antes de mais nada: funcoes sao chamadas durante o programa;
+Voce tambem pode ouvir: executar, chamar, invocar uma funcao;
+E nos chamamos a funcao utilizando o nome dela + abrindo e fechando parenteses;
+Tambem podemos inserir paramentros, em algumas funcoes sao obrigatorios;
+Basicamente sao valores que podem mudar o resultado da funcao;
+*/
+
+/*FUNCAO BUILT IN: console.log()
+Exibe uma mensagem na tela, que é o argumento que passamos
+Ex: console.log('exibindo esta mensagem!')
+Essa funcao é muito utilizada.
+*/
+let idade = 40
+console.log(idade)
+
+let nome = 'Humberto'
+console.log(nome)
+
+console.log(`O meu nome é ${nome} e tenho  ${idade} anos`)
+
