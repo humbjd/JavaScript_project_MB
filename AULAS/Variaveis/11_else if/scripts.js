f4137f2ea35b09b8ee803d0fc7780a6ea1806d13
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2023-05-23 14:45:18 -0300

    continuação do curso

---
+++ AULAS/Variaveis/11_else if/scripts.js
@@ -0,0 +1,10 @@
+let nome = 'Humberto';
+
+if(nome != undefined && nome == 'Joaquim'){
+    console.log('Nome está definido')
+}else if(nome == 'Humberto' && nome.length > 5 && idade == 50){
+    console.log('O nome é Humberto')
+}else{
+    console.log('Não é Humberto')
+}
+
---
+++ AULAS/Variaveis/9_if/scripts.js
@@ -0,0 +1,20 @@
+let idade = 19;
+
+if (idade == 19){
+    console.log('A idade é = 19')
+}
+
+if (idade > 25){
+    console.log('A idade é maior que 25')
+}
+
+let nome = 'Humberto'
+if (idade == 'Humberto' && idade > 10){
+    console.log('Liberado!')
+}
+
+let passaporte = true;
+if ((nome == 'Humberto' && idade > 40) || passaporte == true){
+    console.log('Liberado 2!');
+}
+
