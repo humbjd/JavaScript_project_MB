0a581ee0bdf6a2535e227d5de6b2023aff398da8
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2023-05-23 16:45:24 -0300

    continuacao do curso

---
+++ AULAS/Variaveis/19_switch_case/scripts.js
@@ -0,0 +1,22 @@
+let nome = 'Pedro';
+
+switch(nome){
+    case 'Humberto':
+        console.log('O nome é Humberto')
+        break
+    case 'João':
+        console.log('O nome é João')
+        break
+    default:
+        console.log('O nome não foi encontrado')
+        break        
+}
+
+
+/*
+if(nome == 'Humberto'){
+    console.log('O nome é Humberto')
+}else {
+    console.log('O nome não foi encontrado')
+}
+*/
