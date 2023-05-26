6d81ca9fc1bcae741d21b91a2de1c474ef0b27b6
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2023-05-26 15:03:25 -0300

    continuacao do curso

---
+++ EXERCICIOS/14_ex/scripts.js
@@ -0,0 +1,19 @@
+let num = 0;
+
+while(num <= 50){
+    if(num % 2 == 0){
+        console.log(`${num} é par`)
+        num++
+    }else{
+        console.log(`${num} é ímpar`)
+        num++
+    }
+}
+        
+for(let i = 0; i <= 50; i++){
+    if(i % 2 == 0){
+        console.log(`${i} é par`)
+    }else{
+        console.log(`${i} é ímpar`)
+    }
+}    
---
+++ EXERCICIOS/15_ex/scripts.js
@@ -0,0 +1,22 @@
+
+
+for(let i = 0; i <= 100; i++){
+    if(i <= 1){
+        console.log(`O numero ${i} NÃO é PRIMO`)
+    }else if(i <= 3){
+        console.log(`O numero ${i} NÃO é PRIMO`)
+    }else if(i % 2 == 0 || i % 3 == 0){
+        console.log(`O numero ${i} NÃO é PRIMO`)
+    }else if(let div = 5; div){
+
+    }
+}
+
+
+
+
+
+
+
+console.log(`O numero ${i} é divisível por ${i} e por 1, portanto é um numero PRIMO`)
+
